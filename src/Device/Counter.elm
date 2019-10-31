7dd3575ae213503e65a44c229fc069b321b7036b
Aram <aram.bekverdyan@gmail.com>
2019-10-31 19:08:28 +0400

    feat: counter encoder

--- src/Device/Counter.elm
+++ src/Device/Counter.elm
@@ -1,12 +1,11 @@
-module Device.Counter exposing (Counters)
+module Device.Counter exposing (Counter, encodeCounter)
+
+import Json.Decode as D
+import Json.Encode as E
 
 
-type alias Counters =
-    List Counter
-
-
-type Counter
-    = Counter ( Name, Pointer )
+type alias Counter =
+    ( Name, Pointer )
 
 
 type alias Name =
@@ -23,3 +22,28 @@ type alias Daily =
 
 type alias Total =
     Int
+
+
+
+--ENCODE
+
+
+encodeCounter : Counter -> E.Value
+encodeCounter counter =
+    E.object
+        [ ( "name", E.string <| Tuple.first counter )
+        , ( "pointer", encodePointer <| Tuple.second counter )
+        ]
+
+
+encodeName : Name -> E.Value
+encodeName name =
+    E.object [ ( "name", E.string name ) ]
+
+
+encodePointer : Pointer -> E.Value
+encodePointer pointer =
+    E.object
+        [ ( "daily", E.int (Tuple.first pointer) )
+        , ( "total", E.int (Tuple.second pointer) )
+        ]
