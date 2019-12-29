1d20d7d48ece5aa519a8b2ec83253352da392fb5
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2019-12-29 23:26:15 +0400

    fix: Refactor Channel data structure

--- src/Device/Channel.elm
+++ src/Device/Channel.elm
@@ -1,4 +1,13 @@
-module Device.Channel exposing (Model, Msg, decoder, encode, newChannels, update, view)
+module Device.Channel exposing
+    ( Model
+    , Msg
+    , decoder
+    ,  encode
+       -- , newChannels
+
+    , update
+    , view
+    )
 
 import Html exposing (..)
 import Json.Decode as D
@@ -22,39 +31,44 @@ type alias Defined =
 
 
 type alias Channel =
-    ( Index, List Ingredient )
+    ( Index, List Component )
 
 
 type alias Index =
     Int
 
 
-type alias Ingredient =
-    ( Resource, Portion )
-
-
-type alias Portion =
-    Int
-
-
-type alias Resource =
+type alias Component =
     ( Name, Unit )
-
-
-type alias Unit =
-    String
 
 
 type alias Name =
     String
 
 
+type Unit
+    = Liter Int
+    | Gram Int
+    | Kilowatt Int
+    | Meter Int
 
+
+
+-- type alias Ingredient =
+--     ( Resource, Portion )
+-- type alias Portion =
+--     Int
+--
+-- type alias Resource =
+--     ( Name, Unit )
+--
+-- type alias Unit =
+--     String
 -- CREATE
 
 
-newChannels : Actual -> Model
-newChannels actual =
+init : Actual -> Model
+init actual =
     ( actual, [] )
 
 
@@ -79,7 +93,7 @@ decoder : D.Decoder Model
 decoder =
     D.map2 channelsOf
         (D.field "actual" D.int)
-        (D.field "defined" <| D.list decodeComponent)
+        (D.field "defined" <| D.list decodeChannel)
 
 
 channelsOf : Actual -> Defined -> Model
@@ -87,83 +101,81 @@ channelsOf actual defined =
     ( actual, defined )
 
 
-componentOf : Int -> List Ingredient -> Channel
-componentOf index ingredients =
-    ( index, ingredients )
-
-
-decodeComponent : D.Decoder Channel
-decodeComponent =
-    D.map2 componentOf
+decodeChannel : D.Decoder Channel
+decodeChannel =
+    D.map2 ( Index, unitFrom )
         (D.field "index" D.int)
-        (D.field "ingredients" <| D.list decodeIngredient)
+        (D.field "component" decodeComponent)
 
 
-newIngredient : Resource -> Portion -> Ingredient
-newIngredient resource portion =
-    ( resource, portion )
-
-
-newResource : Name -> Unit -> Resource
-newResource name unit =
-    ( name, unit )
-
-
-decodeIngredient : D.Decoder Ingredient
-decodeIngredient =
-    D.map2 newIngredient
-        (D.field "resource" decodeResource)
-        (D.field "portion" D.int)
-
-
-decodeResource : D.Decoder Resource
-decodeResource =
-    D.map2 newResource
+decodeComponent : D.Decoder Compoent
+decodeComponent =
+    D.map2 ( Name, unitFrom )
         (D.field "name" D.string)
-        (D.field "unit" D.string)
+        (D.field "unit" decodeUnit)
+
+
+decodeUnit : D.Decoder Unit
+decodeUnit =
+    D.map2 unitFrom
+        (D.field "name" D.string)
+        (D.field "value" D.int)
+
+
+unitFrom : String -> Int -> Maybe Unit
+unitFrom name value =
+    case name of
+        "liter" ->
+            Just <| Liter value
+
+        "gram" ->
+            Just <| Gram value
+
+        "kilowatt" ->
+            Just <| Kilowatt value
+
+        "meter" ->
+            Just <| Meter value
+
+        _ ->
+            Nothing
 
 
 
 -- ENCODE
 
 
-encodeResource : Resource -> E.Value
-encodeResource resource =
-    E.object
-        [ ( "name", E.string <| Tuple.first resource ) ]
-
-
-encodeIngredient : Ingredient -> E.Value
-encodeIngredient ingredient =
-    E.object
-        [ ( "resource", encodeResource <| Tuple.first ingredient )
-        , ( "portion", E.int <| Tuple.second ingredient )
-        ]
-
-
-encodeChannel : Channel -> E.Value
-encodeChannel channel =
-    E.object
-        [ ( String.fromInt <| Tuple.first channel
-          , E.list encodeIngredient <| Tuple.second channel
-          )
-        ]
-
-
-encodeDefined : Defined -> E.Value
-encodeDefined defined =
-    E.list encodeChannel defined
-
-
 encode : Model -> E.Value
 encode ( actual, defined ) =
     E.object
         [ ( "actual", E.int actual )
-        , ( "defined", encodeDefined defined )
+
+        -- , ( "defined", E.list <| encodeComponent defined )
         ]
 
 
 
+--
+-- encodeResource : Resource -> E.Value
+-- encodeResource resource =
+--     E.object
+--         [ ( "name", E.string <| Tuple.first resource ) ]
+--
+--
+-- encodeIngredient : Ingredient -> E.Value
+-- encodeIngredient ingredient =
+--     E.object
+--         [ ( "resource", encodeResource <| Tuple.first ingredient )
+--         , ( "portion", E.int <| Tuple.second ingredient )
+--         ]
+-- encodeComponent : Component -> E.Value
+-- encodeComponent (name, unit) =
+-- case
+-- E.object
+--     [ ( String.fromInt <| Tuple.first channel
+--       , E.list encodeIngredient <| Tuple.second channel
+--       )
+--     ]
 -- VIEW
 
 
