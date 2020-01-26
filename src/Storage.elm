858d1b04933caf2754922407c13b149abc72e96d
Aram Bekverdyan <aram.bekverdyan@gmail.com>
2020-01-27 00:41:26 +0400

    feat: Row implementation of Port module

---
+++ src/Storage.elm
@@ -0,0 +1,188 @@
+port module Storage exposing
+    ( Model
+    , loadBranches
+    , loadDevices
+    , pushBranches
+    , pushDevices
+    )
+
+import Branch
+import Device
+import Dict exposing (Dict)
+import Json.Decode as D
+import Json.Encode as E
+
+
+
+-- PORT
+
+
+port saveBranches : E.Value -> Cmd msg
+
+
+port saveDevices : E.Value -> Cmd msg
+
+
+port loadBranches : (E.Value -> msg) -> Sub msg
+
+
+port loadDevices : (E.Value -> msg) -> Sub msg
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { devices : Devices
+    , branches : Branches
+    }
+
+
+
+-- ALIAS
+
+
+type alias Branches =
+    Dict Branch.Identifier Branch.Branch
+
+
+type alias Devices =
+    Dict Device.Identifier Device.Device
+
+
+
+-- UPDATE
+
+
+type Msg
+    = PullDevices E.Value
+    | PullBranches E.Value
+    | PushDevices
+    | PushBranches
+    | AddDevice ( Device.Device, Branch.Branch )
+
+
+update : Msg -> Model -> ( Model, List (Cmd Msg) )
+update msg model =
+    case msg of
+        PullDevices encoded ->
+            ( { model
+                | devices = decodeDevices encoded
+              }
+            , Cmd.none
+            )
+
+        PullBranches encoded ->
+            ( { model
+                | branches = decodeBranches encoded
+              }
+            , Cmd.none
+            )
+
+        PushDevices ->
+            ( model
+            , saveDevices <|
+                encodeDevices model.devices
+            )
+
+        PushBranches ->
+            ( model
+            , saveBranches <|
+                encodeBranches model.branches
+            )
+
+        AddDevice ( device, container ) ->
+            let
+                devices =
+                    model.devices
+
+                branches =
+                    model.branches
+            in
+            ( { model
+                | devices =
+                    Dict.insert
+                        device.id
+                        device
+                        devices
+                , branches =
+                    Dict.insert
+                        container.id
+                        container
+                        branches
+              }
+            , Cmd.batch [ PushDevices, PushBranches ]
+            )
+
+
+
+-- WRITE
+
+
+pushBranches : Maybe Branches -> Cmd msg
+pushBranches dashboard =
+    case dashboard of
+        Just branches ->
+            saveBranches <| encodeBranches branches
+
+        Nothing ->
+            Cmd.none
+
+
+pushDevices : Maybe Devices -> Cmd msg
+pushDevices devices =
+    case devices of
+        Just value ->
+            saveDevices <| encodeDevices value
+
+        Nothing ->
+            Cmd.none
+
+
+
+-- ENCODE
+
+
+encodeBranches : Branches -> E.Value
+encodeBranches branches =
+    E.dict Branch.idToString Branch.encode branches
+
+
+encodeDevices : Devices -> E.Value
+encodeDevices devices =
+    E.dict Device.idToString Device.encode devices
+
+
+
+-- DECODER
+
+
+decodeDevices : D.Value -> Devices
+decodeDevices encoded =
+    let
+        deviceDecoder : D.Decoder Devices
+        deviceDecoder =
+            D.dict Device.decoder
+    in
+    case D.decodeValue deviceDecoder encoded of
+        Ok value ->
+            value
+
+        Err message ->
+            Dict.empty
+
+
+decodeBranches : D.Value -> Branches
+decodeBranches encoded =
+    let
+        decoder : D.Decoder Branches
+        decoder =
+            D.dict Branch.decoder
+    in
+    case D.decodeValue decoder encoded of
+        Ok branches ->
+            branches
+
+        Err message ->
+            Dict.empty
